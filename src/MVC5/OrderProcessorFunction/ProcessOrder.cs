ad90da17be8af9b9ac7c8d62de24a5bc22604eb0
Paul Yuknewicz <paulyuk@microsoft.com>
2024-05-16 10:43:44 -0700

    Added OrderProcessorFunction that is compatible with /api/cart/{shoppingCartId}/order API route

---
+++ src/MVC5/OrderProcessorFunction/ProcessOrder.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+
+namespace OrderProcessorFunction
+{
+    public class ProcessOrder
+    {
+        private readonly ILogger<ProcessOrder> _logger;
+
+        public ProcessOrder(ILogger<ProcessOrder> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("order")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "cart/accounts/{shoppingCartId}/order")] HttpRequest req, [FromBody] Order order, string shoppingCartId)
+        {
+            _logger.LogInformation($"Received order with shoppingCardId: {shoppingCartId} from name:{order.FirstName} {order.LastName} ");
+            _logger.LogInformation(order.ToString());
+            return new OkObjectResult(order);
+        }
+    }
+        public record Order(string LastName, string FirstName, string Address, string City, string State, string PostCode, string Country, string Email, string Phone, string Username);
+
+}
